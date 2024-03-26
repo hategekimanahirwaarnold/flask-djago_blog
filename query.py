93147c8a001f498c67d50458918f4f7d89a3c91d
Hirwa Arnold <macbook@macbooks-MacBook-Pro.local>
2024-03-26 09:21:45 +0200

    API pagination

--- query.py
+++ query.py
@@ -3,5 +3,7 @@ from flaskblog import app, db
 from flaskblog.models import User, Post
 
 with app.app_context():
-   user = User.query.all()
-   print(user)
+   posts = Post.query.paginate(per_page=5, page=2)
+   for post in posts.items:
+      print(f"{posts.page}, post: {post}")
+
