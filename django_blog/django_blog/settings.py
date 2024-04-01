f45d3bcc85af65cf600a7a175764a9bc5e31b4dd
Hirwa Arnold <macbook@macbooks-MacBook-Pro.local>
2024-04-01 22:05:20 +0200

    Reset password - part 2

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -141,8 +141,13 @@ LOGIN_URL = 'login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL')
-EMAIL_HOST_PASSWORD = os.getenv('PASS')
+EMAIL_HOST_USER = os.environ.get('EMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('PASS')
+
+print(os.getenv('DISABLE_SSL_CERTIFICATE_VERIFICATION'))
+print("pass: ", EMAIL_HOST_PASSWORD)
+print("email: ", EMAIL_HOST_USER)
